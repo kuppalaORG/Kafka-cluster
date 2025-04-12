65daf96bdf24c325a0194abe7b4d4b927a950433
Bharath <bharathkumarkuppala@gmail.com>
2025-04-12 23:17:58 +0530

    added state

--- main.tf
+++ main.tf
@@ -3,3 +3,13 @@ module "vpc" {
   name       = "kafka-vpc"
   cidr_block = "10.0.0.0/16"
 }
+
+data "aws_availability_zones" "available" {}
+
+module "subnets" {
+  source               = "./modules/subnet"
+  vpc_id               = module.vpc.vpc_id
+  name_prefix          = "kafka"
+  availability_zones   = data.aws_availability_zones.available.names
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
---
+++ modules/subnet/main.tf
@@ -0,0 +1,13 @@
+resource "aws_subnet" "public" {
+  count               = length(var.public_subnet_cidrs)
+  vpc_id              = var.vpc_id
+  cidr_block          = var.public_subnet_cidrs[count.index]
+  availability_zone   = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    name = "${var.name_prefix}-public-subnet-${count.index+1}"
+  }
+
+
+}
