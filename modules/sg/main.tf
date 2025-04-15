7b993d3096c61dca2f83b0d59b6bcf25b3bd03e8
bharathkuppala <bharath.kuppala@simplifyvms.com>
2025-04-15 14:40:58 +0530

    added DNS

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -3,6 +3,7 @@ resource "aws_security_group" "kafka_sg" {
   description = "Allow Kafka, Zookeeper, and SSH"
   vpc_id      = var.vpc_id
 
+  # SSH access
   ingress {
     description = "Allow SSH"
     from_port   = 22
@@ -11,6 +12,7 @@ resource "aws_security_group" "kafka_sg" {
     cidr_blocks = var.allowed_cidrs
   }
 
+  # Kafka client access
   ingress {
     description = "Allow Kafka"
     from_port   = 9092
@@ -19,12 +21,40 @@ resource "aws_security_group" "kafka_sg" {
     cidr_blocks = var.allowed_cidrs
   }
 
+  # Zookeeper client access
   ingress {
-    description = "Allow Zookeeper"
+    description = "Allow Zookeeper client connections"
     from_port   = 2181
     to_port     = 2181
     protocol    = "tcp"
     cidr_blocks = var.allowed_cidrs
+  }
+
+  # ZooKeeper quorum sync (intra-broker)
+  ingress {
+    description = "Zookeeper quorum sync"
+    from_port   = 2888
+    to_port     = 2888
+    protocol    = "tcp"
+    self        = true
+  }
+
+  # ZooKeeper leader election (intra-broker)
+  ingress {
+    description = "Zookeeper leader election"
+    from_port   = 3888
+    to_port     = 3888
+    protocol    = "tcp"
+    self        = true
+  }
+
+  # Kafka inter-broker communication (optional if using multiple brokers)
+  ingress {
+    description = "Kafka inter-broker communication"
+    from_port   = 9092
+    to_port     = 9092
+    protocol    = "tcp"
+    self        = true
   }
 
   egress {
--- modules/sg/output.tf
+++ modules/sg/output.tf
@@ -1,3 +1,19 @@
 output "security_group_id" {
   value = aws_security_group.kafka_sg.id
 }
+
+output "security_group_name" {
+  value = aws_security_group.kafka_sg.name
+}
+
+output "security_group_vpc_id" {
+  value = aws_security_group.kafka_sg.vpc_id
+}
+
+output "security_group_ingress_rules" {
+  value = aws_security_group.kafka_sg.ingress
+}
+
+output "security_group_egress_rules" {
+  value = aws_security_group.kafka_sg.egress
+}
